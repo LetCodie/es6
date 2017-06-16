4979762d3bd4cfa6bf29ec08010974922350033b
radin-reth <radin.reth@gmail.com>
2017-06-16 22:10:31 +0700

    proxy

--- src/proxies.js
+++ src/proxies.js
@@ -8,4 +8,23 @@ var handler = {
 
 var p = new Proxy(target, handler);
 
-console.log(p.cambodia)
+//console.log(p.cambodia)
+
+let { op: a, lhs: { op: b }, rhs: c } = { op: 1, lhs: { op: 2 }, rhs: 3 }
+
+
+//console.log(`a = ${a} `)
+let obj = { a: 1}
+function f ([ name, val ]) {
+    console.log(name, val)
+}
+function g ({ name: n, val: v }) {
+    console.log(n, v)
+    console.log(arguments[0])
+}
+function h ({ name, val }) {
+    console.log(name, val)
+}
+f([ "bar", 42 ])
+g({ name: "foo", val:  7 })
+h({ name: "bar", val: 42 })
